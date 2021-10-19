2ca13fed80d5b0670d9a5dd49dbfb82877a6833b
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-10-19 15:43:21 -0300

    corrigido bugs e adicionado testes

--- test/utils.test.js
+++ test/utils.test.js
@@ -10,10 +10,39 @@ describe('Utils', () => {
         });
     });
 
+    describe('gerarNumeroAleatorio', () => {
+        test('inicio nao pode ser negativo', () => {
+            expect(gerarNumeroAleatorio(-2, 5))
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('um intervalo válido [200, 3000] é fornecido. ', () => {
+            const resultado = gerarNumeroAleatorio(200,3000);
+            if(resultado < 200 || resultado > 3000)
+                throw new Error('Valor fora do intervalo');
+        });
+    });
+
     describe('acharCaracter b', () => {
         test('acharCaracter b', () => {
             expect(acharCaracter(3, 'abc', 'b'))
+                .toBe(1);
+        });
+    });
+
+    describe('acharCaracter c', () => {
+        test('acharCaracter c', () => {
+            expect(acharCaracter(3, 'abc', 'c'))
                 .toBe(2);
+        });
+    });
+
+    describe('acharCaracter d', () => {
+        test('acharCaracter d', () => {
+            expect(acharCaracter(3, 'abc', 'd'))
+                .toBe('caracter nao encontrado');
         });
     });
 
